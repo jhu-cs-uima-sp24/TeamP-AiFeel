3be7eab939d801c38b626cdd46e1b47084721204
lisaqhou4 <qhou4@jhu.edu>
2024-04-05 15:58:54 -0400

    add SignupActivity

--- app/src/main/java/com/example/a5_sample/SignupActivity.java
+++ app/src/main/java/com/example/a5_sample/SignupActivity.java
@@ -1,18 +1,16 @@
 
 package com.example.a5_sample;
 
-        import android.content.Context;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.content.Intent;
-        import android.widget.EditText;
-        import android.widget.ImageButton;
-        import android.widget.TextView;
-
-
-        import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
     private SharedPreferences myPrefs;
