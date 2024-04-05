b4a6d67089cfe83b9edb4d79e5bbff4fd7c18759
Jonathan He <d3rk12345678@gmail.com>
2024-04-05 03:02:36 -0400

    Working calendar implementation

--- app/src/main/java/com/example/a5_sample/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a5_sample/ui/home/HomeFragment.java
@@ -1,56 +1,94 @@
 package com.example.a5_sample.ui.home;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.a5_sample.MainActivity;
-import com.example.a5_sample.NewRoom;
 import com.example.a5_sample.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class HomeFragment extends Fragment {
-    private ListView myList;
-    private MainActivity myact;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
-    public static final int MENU_ITEM_EDITVIEW = Menu.FIRST;
-    public static final int MENU_ITEM_DELETE = Menu.FIRST + 1;
+public class HomeFragment extends Fragment implements CalendarAdapter.OnItemListener {
 
-    Context cntx;
- //   Park current;
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // NOTE: HomeViewModel elements eliminated from original template
-
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myview = inflater.inflate(R.layout.fragment_home, container, false);
 
-        cntx = getActivity().getApplicationContext();
-        myact = (MainActivity) getActivity();
+        calendarRecyclerView = myview.findViewById(R.id.calendarRecyclerView);
+        monthYearText = myview.findViewById(R.id.monthYearTV);
+        selectedDate = LocalDate.now();
 
+        // Set up buttons for navigating months
+        myview.findViewById(R.id.prevMonthButton).setOnClickListener(v -> previousMonthAction());
+        myview.findViewById(R.id.nextMonthButton).setOnClickListener(v -> nextMonthAction());
 
-        myact.roomsAdapter.notifyDataSetChanged();
-
-        FloatingActionButton fab = (FloatingActionButton) myview.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent launch = new Intent(myact, NewRoom.class);
-                startActivity(launch);
-            }
-        });
-
+        setMonthView();
         return myview;
     }
 
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private void previousMonthAction() {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    private void nextMonthAction() {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for (int i = 1; i <= 42; i++) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add("");
+            } else {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if (!dayText.equals("")) {
+            LocalDate date = LocalDate.of(selectedDate.getYear(), selectedDate.getMonth(), Integer.parseInt(dayText));
+            Toast.makeText(getContext(), "Selected Date: " + date.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
 }
