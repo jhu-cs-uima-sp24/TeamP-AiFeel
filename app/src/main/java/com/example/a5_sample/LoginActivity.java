7cbe7defbe006351a18ffd38834bfc437cfcf339
ericzhy0815 <69284146+ericzhy0815@users.noreply.github.com>
2024-04-05 20:22:49 -0400

    fixed login layout fixed margin issues with the top action bar

--- app/src/main/java/com/example/a5_sample/LoginActivity.java
+++ app/src/main/java/com/example/a5_sample/LoginActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
--- app/src/main/java/com/example/a5_sample/MainActivity.java
+++ app/src/main/java/com/example/a5_sample/MainActivity.java
@@ -24,26 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    public static ArrayList<JournalEntry> rooms;
-    public static StudyRoomAdapter roomsAdapter;
     public JournalEntry current;
-    public Random randy = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        rooms = new ArrayList<JournalEntry>();
-        rooms.add(new JournalEntry("Malone 228" , 30));
-        rooms.add(new JournalEntry("Malone 107", 20));
-        rooms.add(new JournalEntry("Hackerman 306", 12));
-        rooms.add(new JournalEntry("Dorm 101", 1)); // to test room full
-        roomsAdapter = new StudyRoomAdapter(this, R.layout.roomlayout, rooms);
         current = null;
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        //BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_profile, R.id.navigation_home, R.id.navigation_chat, R.id.navigation_stats)
                 .build();
