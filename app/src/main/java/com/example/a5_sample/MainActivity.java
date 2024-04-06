a93baf95331a4b592ebe87866a0ff346beb78c9f
ericzhy0815 <69284146+ericzhy0815@users.noreply.github.com>
2024-04-05 20:25:43 -0400

    marged main activity changes

--- app/src/main/java/com/example/a5_sample/MainActivity.java
+++ app/src/main/java/com/example/a5_sample/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         //BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_profile, R.id.navigation_home, R.id.navigation_chat, R.id.navigation_stats)
+                R.id.navigation_profile, R.id.navigation_home, R.id.navigation_chat, R.id.navigation_journalEntry)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
